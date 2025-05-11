609fadbde61044b8749bc4b3dfb84a6778f93a93
kevins981 <kevinsong981@gmail.com>
2025-05-10 22:27:17 -0400

    Add retry for centml api

--- llms/providers/centml_utils.py
+++ llms/providers/centml_utils.py
@@ -11,48 +11,52 @@ import openai
 #import openai.error
 
 
-#def retry_with_exponential_backoff(  # type: ignore
-#    func,
-#    initial_delay: float = 1,
-#    exponential_base: float = 2,
-#    jitter: bool = True,
-#    max_retries: int = 3,
-#    errors: tuple[Any] = (openai.error.RateLimitError,),
-#):
-#    """Retry a function with exponential backoff."""
-#
-#    def wrapper(*args, **kwargs):  # type: ignore
-#        # Initialize variables
-#        num_retries = 0
-#        delay = initial_delay
-#
-#        # Loop until a successful response or max_retries is hit or an exception is raised
-#        while True:
-#            try:
-#                return func(*args, **kwargs)
-#            # Retry on specified errors
-#            except errors as e:
-#                # Increment retries
-#                num_retries += 1
-#
-#                # Check if max retries has been reached
-#                if num_retries > max_retries:
-#                    raise Exception(
-#                        f"Maximum number of retries ({max_retries}) exceeded."
-#                    )
-#
-#                # Increment the delay
-#                delay *= exponential_base * (1 + jitter * random.random())
-#                print(f"Retrying in {delay} seconds.")
-#                # Sleep for the delay
-#                time.sleep(delay)
-#
-#            # Raise exceptions for any errors not specified
-#            except Exception as e:
-#                raise e
-#
-#    return wrapper
+def retry_with_exponential_backoff(  # type: ignore
+    func,
+    initial_delay: float = 1,
+    exponential_base: float = 2,
+    jitter: bool = True,
+    max_retries: int = 3,
+    #errors: tuple[Any] = (,),
+):
+    """Retry a function with exponential backoff."""
+    print("=== in retry_with_exponential_backoff")
 
+    def wrapper(*args, **kwargs):  # type: ignore
+        # Initialize variables
+        num_retries = 0
+        delay = initial_delay
+
+        # Loop until a successful response or max_retries is hit or an exception is raised
+        while True:
+            try:
+                return func(*args, **kwargs)
+            # Retry on specified errors
+            #except errors as e:
+            except Exception as e:
+                # Increment retries
+                num_retries += 1
+                print("=== retrying centml api call. error: ", e)
+
+                # Check if max retries has been reached
+                if num_retries > max_retries:
+                    raise Exception(
+                        f"Maximum number of retries ({max_retries}) exceeded."
+                    )
+
+                # Increment the delay
+                delay *= exponential_base * (1 + jitter * random.random())
+                print(f"Retrying in {delay} seconds.")
+                # Sleep for the delay
+                time.sleep(delay)
+
+            # Raise exceptions for any errors not specified
+            #except Exception as e:
+            #    raise e
+
+    return wrapper
+
+@retry_with_exponential_backoff
 def generate_from_centml_chat_completion(
     messages: list[dict[str, str]],
     model: str,
